f911b3f803f38de852e1febada1e09adb29a5a5d
venu <venumannem12018@gmail.com>
2023-07-10 15:42:28 +0530

    created Header

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <p> lorem ipsm lorem ipsm lorem ipsm lorem ipsm lorem ipsm lorem ipsm</p>
+    </div>
+  );
+};
+
+export default Header;
