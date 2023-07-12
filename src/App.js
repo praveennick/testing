64d355e41a7f91f05bcce537a0fcc22a70296b04
venu <venumannem12018@gmail.com>
2023-07-12 11:35:56 +0530

    added margin

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h4>Hi react js file</h4>
+      <h4 style={{ margin: "20px" }}>Hi react js file</h4>
     </div>
   );
 }
